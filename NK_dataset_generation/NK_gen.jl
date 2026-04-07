0d07df7a17fec641eca25a3314a191077737972b
alfkristian02 <alfkristian02@gmail.com>
2026-04-07 22:26:20 +0200

    feat: finish genertion of nk dataset files

--- NK_dataset_generation/NK_gen.jl
+++ NK_dataset_generation/NK_gen.jl
@@ -1,20 +1,20 @@
 using StatsBase
 using Random
 
-Random.seed!(42) # for reproducibility
+Random.seed!(42) # 42 is always the answer
 
-N::Int64 = 5 #number of features
-K::Int64 = 2 #number of neighborhood
+N::Int64 = 25 #number of features
+K::Int64 = 24 #size of neighborhood
 
 
-random_neighborhood = Dict{Int, Vector{Int}}()
+interaction_sets = Dict{Int, Vector{Int}}()
 fitness_component_tables = Dict{Int, Vector{Float64}}()
 
 for i = 1:N
     options = setdiff(1:N, [i]) #create the set of options
     neighbors = sample(options, K; replace=false) # select the neighborhood, no duplicates
 
-    random_neighborhood[i] = neighbors;
+    interaction_sets[i] = [i; sort(neighbors)] # consistent ordering
 
     # fill the fitness component tables here as well
     fitness_component_tables[i] = rand(2^(K+1))
@@ -24,17 +24,47 @@ function binary_to_decimal(bit_vector)
     return sum(bit_vector .* 2 .^(length(bit_vector) .- eachindex(bit_vector))) + 1 # need to correct for Julia's 1-indexing
 end
 
-function fitness_function(x::BitVector)
+function fitness_function(x::BitVector, interaction_sets, tables, N)
     
-    running_sum = 0
+    running_sum = .0
 
-    for (key, value) in random_neighborhood
-        running_sum += fitness_component_tables[key][binary_to_decimal(x[[key, value...]])]
+    for i in 1:N
+        idx = interaction_sets[i] # is ordered
+        running_sum += tables[i][binary_to_decimal(x[idx])]
     end
 
     return 1/N * running_sum
 end
 
-println(fitness_function(BitVector([1, 1, 1, 1, 1])))
+function decimal_to_binary(decimal::Int, length::Int)
+    return BitVector(((decimal >> (length - i)) & 1) == 1 for i in 1:length)
+end
 
-# now, next step is to go over all bitstrings, run the fitness function, and then save it to a table that can be used as precomputed. coolcool
+res = Vector{Float64}(undef, 2^N)
+
+for i = 0:(2^N-1)
+    res[i+1] = fitness_function(decimal_to_binary(i, N), interaction_sets, fitness_component_tables, N)
+end
+
+# sanity checks
+# println(res)
+# println(minimum(res))
+# println(maximum(res))
+# println(length(res) == 2^N)
+# println(mean(res))
+
+struct NKLandscape
+    table::Vector{Float64}
+    N::Int
+    K::Int
+end
+
+nk = NKLandscape(res, N, K)
+
+using JLD2
+path = "NK_dataset_generation/precomputed_synthetic/nk_K$(K).jld2"
+@save path nk
+
+# @load path nk
+
+# print(nk)
--- config.jl
+++ config.jl
@@ -1,7 +1,8 @@
 module ConfigParameters
     export dataset_file_name, fitness_function, population_size, number_of_generations, mutation_rate, local_search_frequencies, local_search_depths, save_run, crossover_probability, sls_p
     
-    const dataset_file_name::String = "05-heart-c_dt_mat-1.jld2" # 05-heart-c_dt_mat-1.jld2 || 07-credit-a_dt_matG.jld2 || 10-hepatitis_dt_matG.jld2
+    # 05-heart-c_dt_mat-1.jld2 || 07-credit-a_dt_matG.jld2 || 10-hepatitis_dt_matG.jld2
+    const dataset_file_name::String = "05-heart-c_dt_mat-1.jld2" 
     
     const EPSILON::Float64 = 0.001
 
@@ -9,20 +10,19 @@ module ConfigParameters
         if individual_decimal == 0
             return .0
         end
-        
         return base_fitness_vector[individual_decimal] - EPSILON * sum(individual_binary)
     end
 
-    # local_search_frequencies::Vector{Int} = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 25, 50, 75, 100, 150, 250, 500]
     local_search_frequencies::Vector{Float64} = [.0, 1.0]
     local_search_depths::Vector{Int} = [1]
+    sls_p::Float64 = 0.1 # make hill climbing with a probability of executing
 
+    # could add initialization variable. and implement one thats a bit more deterministic. better stability. e.g evenly distributed in the search space.
     population_size::Int = 250
-    number_of_generations::Int = 5
+    number_of_generations::Int = 5 # Maybe number of fitness evaluations is a better alternative. It is what the analysis will use.
     crossover_probability::Float64 = 0.25
     mutation_rate::Float64 = 0.05
 
     save_run::Bool = 0
 
-    sls_p::Float64 = 0.1
 end

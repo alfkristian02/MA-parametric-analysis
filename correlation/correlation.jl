a1401e6f05c04bb854513846a0bd837481fe0a40
alfkristian02 <alfkristian02@gmail.com>
2026-05-02 16:13:58 +0200

    feat: correlation in accumulated data

---
+++ correlation/correlation.jl
@@ -0,0 +1,46 @@
+using DataFrames, StatsBase, CSV, Plots
+
+# might want to make a type for this
+
+# gather data
+files::Vector{String} = ["heart_13_04212141.csv", "letter_16_04251835.csv", "letter_16_04251835.csv", "nk_K2_04230937.csv", "nk_K9_04251941.csv", "nk_K16_04251941.csv"]
+included_columns::Vector{String} = ["ds", "population_size", "number_of_generations", "crossover_probability", "mutation_rate", "ls_p_value", "ls_max_steps", "avg_ga_improvement", "avg_ls_improvement", "best_found", "fitness_function_accesses"]
+
+load_data = [select(CSV.read(joinpath("runs", file), DataFrame), included_columns) for file in files]
+
+combined = reduce(vcat, load_data, cols=:union)
+
+# print(nrow(combined))
+
+# one hot encode dataset
+for ds in unique(combined.ds)
+    combined[!, Symbol(ds)] = [x==ds ? 1 : 0 for x in combined.ds]
+end
+
+numeric_df = select(combined, Not(:ds))
+
+# handle NaN values (from when the pops are initialised with the best solution)
+cols_to_fix = [:avg_ga_improvement, :avg_ls_improvement] 
+
+for col in cols_to_fix
+    # replace!() modifies the column in-place
+    replace!(numeric_df[!, col], NaN => 0.0)
+end
+
+# for all pairs of variables compute pearson correlation coefficients
+correlation_matrix = cor(Matrix(numeric_df))
+
+# print it in some format
+# print(correlation_matrix)
+
+labels = names(numeric_df)
+
+display(heatmap(
+    labels,
+    labels,
+    correlation_matrix,
+    xrotation = 45,
+    size =(900, 800),
+))
+
+readline()

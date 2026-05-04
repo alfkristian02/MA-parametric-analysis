2faaf62a9c15c7e54fba400ad25010273de2fb92
alfkristian02 <alfkristian02@gmail.com>
2026-05-04 11:56:41 +0200

    chore: -

--- causal_inference/correlation.jl
+++ causal_inference/correlation.jl
@@ -3,8 +3,8 @@ using DataFrames, StatsBase, CSV, Plots
 # might want to make a type for this
 
 # gather data
-files::Vector{String} = ["heart_13_04212141.csv", "letter_16_04251835.csv", "letter_16_04251835.csv", "nk_K2_04230937.csv", "nk_K9_04251941.csv", "nk_K16_04251941.csv"]
-included_columns::Vector{String} = ["ds", "population_size", "number_of_generations", "crossover_probability", "mutation_rate", "ls_p_value", "ls_max_steps", "avg_ga_improvement", "avg_ls_improvement", "best_found", "fitness_function_accesses"]
+files::Vector{String} = ["nk_K16.csv", "nk_K2.csv", "nk_K9.csv", "heart_13.csv", "letter_16.csv", "hep_19.csv"]
+included_columns::Vector{String} = ["population_size", "number_of_generations", "crossover_probability", "mutation_rate", "ls_p_value", "ls_max_steps", "avg_ga_improvement", "avg_ls_improvement", "best_found", "fitness_function_accesses", "ds_diff"]
 
 load_data = [select(CSV.read(joinpath("runs", file), DataFrame), included_columns) for file in files]
 
@@ -13,27 +13,27 @@ combined = reduce(vcat, load_data, cols=:union)
 # print(nrow(combined))
 
 # one hot encode dataset
-for ds in unique(combined.ds)
-    combined[!, Symbol(ds)] = [x==ds ? 1 : 0 for x in combined.ds]
-end
+# for ds in unique(combined.ds)
+#     combined[!, Symbol(ds)] = [x==ds ? 1 : 0 for x in combined.ds]
+# end
 
-numeric_df = select(combined, Not(:ds))
+# numeric_df = select(combined, Not(:ds))
 
 # handle NaN values (from when the pops are initialised with the best solution)
 cols_to_fix = [:avg_ga_improvement, :avg_ls_improvement] 
 
 for col in cols_to_fix
     # replace!() modifies the column in-place
-    replace!(numeric_df[!, col], NaN => 0.0)
+    replace!(combined[!, col], NaN => 0.0)
 end
 
 # for all pairs of variables compute pearson correlation coefficients
-correlation_matrix = cor(Matrix(numeric_df))
+correlation_matrix = cor(Matrix(combined))
 
 # print it in some format
 # print(correlation_matrix)
 
-labels = names(numeric_df)
+labels = names(combined)
 
 display(heatmap(
     labels,
